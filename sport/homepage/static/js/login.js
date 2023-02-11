7fa1eb06e6be3fd85b0e50c105f3297473670896
miximc <miximc@mail.ru>
2023-02-11 13:41:07 +0300

    Добавления авторизации

--- sport/homepage/static/js/login.js
+++ sport/homepage/static/js/login.js
@@ -2,3 +2,9 @@ function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
+
+function register() {
+    var tag = '<div></div>';
+    var target = document.getElementById('okno');
+    target.innerHTML = tag;
+}
--- sport/homepage/views.py
+++ sport/homepage/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
 
 def index(request):
-    context = {
-        'nomer_okowko': 25,
-        'phone': 2,
-        'slovar': [1,2,3,4,5,6]}
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'],
+            password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Успешная авторизация')
+                else:
+                    return HttpResponse('Дизайблед аккаунт')
+            else:
+                return('Invalid login')
+    else:
+        form = LoginForm()
 
-    return render(
-        request,                # Запрос
-        'homepage/index.html',
-        context,                 # подстановки
+    return render(request, 'homepage/index.html', {'form': form})        
+#def index(request):
+#    context = {
+#        'nomer_okowko': 25,
+#        'phone': 2,
+#        'slovar': [1,2,3,4,5,6]}
+
+#    return render(
+#        request,                # Запрос
+#        'homepage/index.html',
+#        context,                 # подстановки
 
 
-    )
+#    )
